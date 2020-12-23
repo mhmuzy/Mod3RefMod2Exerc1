41178bde5d215ff3e0f1601cc6b285922973bd04
marcio henrique <mhmuzy@embratel.com.br>
2020-12-23 12:31:53 -0300

    Criação do Programa que faz a soma dos números [Treinamento]

---
+++ src/Hitss/Treinamento/Apresentacao/Show.java
@@ -0,0 +1,12 @@
+package Hitss.Treinamento.Apresentacao;
+
+public class Show {
+	public static void main(String[] args) {
+		try {
+			System.out.println("");
+			System.out.println("     Hitss Treinamento");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
